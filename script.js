a175dc423c067bcd4883d19940f0700867b11de3
mehabubi-meh <mehabubimeh0708@gmail.com>
2025-06-17 13:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	function trailingZeros(n) {
+  n = parseInt(n);
+  let count = 0;
+  while (n >= 5) {
+    n = Math.floor(n / 5);
+    count += n;
+  }
+  return count;
 }
 
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
+
+}
+
+const input = prompt("Enter a number");
+//alert(trailingZeros(input));
